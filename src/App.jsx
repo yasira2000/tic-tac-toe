3fc5594117426d4a0d2900b130df44c104f2ffe7
yasira2000 <yasirapunsith@gmail.com>
2024-10-30 08:30:19 +0530

    add jump to function to game

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares, move) => {
@@ -23,8 +25,10 @@ export default function Game() {
       description = "Go to game start";
     }
     return (
-      <li>
-        <button className="move-button" onClick={() => jumpTo(move)}>{description}</button>
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {description}
+        </button>
       </li>
     );
   });
